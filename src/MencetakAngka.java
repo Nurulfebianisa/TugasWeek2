052f73ef53394061edfcb9e74ed050c85159ad5f
Nurulfebianisa <92684819+Nurulfebianisa@users.noreply.github.com>
2021-11-10 11:06:46 +0700

    Add files via upload

---
+++ src/MencetakAngka.java
@@ -0,0 +1,22 @@
+public class MencetakAngka {
+    public static void main(String[] args) {
+
+        int[] nilai;
+
+        nilai = new int[6];
+
+        nilai[0] = 6;
+        nilai[1] = 8;
+        nilai[2] = 10;
+        nilai[3] = 12;
+        nilai[4] = 14;
+        nilai[5] = 16;
+
+        System.out.println(nilai[0]);
+        System.out.println(nilai[1]);
+        System.out.println(nilai[2]);
+        System.out.println(nilai[3]);
+        System.out.println(nilai[4]);
+        System.out.println(nilai[5]);
+    }
+}
