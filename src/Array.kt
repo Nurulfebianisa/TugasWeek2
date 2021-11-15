5e7c1745a0f7c35f8feec846b964a2fd2320e53b
Nurulfebianisa <92684819+Nurulfebianisa@users.noreply.github.com>
2021-11-15 17:28:35 +0700

    Add files via upload

---
+++ src/Array.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val numArr1 = arrayOf(6, 8, 10, 12, 14, 16 )
+    for (i in 0..numArr1.size - 1) {
+        print(" " + numArr1[1])
+        println()
+        val numArr2 = arrayOf(6, 8, 10, 12, 14, 16 )
+        for (i in 0..numArr2.size - 1) {
+            print(" " + numArr2[i])
+        }
+    }
+}
